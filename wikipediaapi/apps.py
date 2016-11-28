83b2d7c387d5a048cb7f8eeabb3fe016394c59a1
Joe <zz3430gs@go.minneapolis.edu>
2016-11-27 22:29:09 -0600

    added the views and other things to git

---
+++ wikipediaapi/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WikipediaapiConfig(AppConfig):
+    name = 'wikipediaapi'
---
+++ wikipediaapi/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from wikipediaApi import *
+# Create your views here.
+def search(request):
+    return render(request)
+
+def summary(request):
+    return render(request)
